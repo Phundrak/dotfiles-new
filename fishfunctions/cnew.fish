7a1d9fdebf6df7b6e52c8daf09b44818a0cf292b
Phuntsok Drak-pa <phundrak@phundrak.fr>
2019-03-26 12:43:45 +0100

    pulled dev and fish functions from thinkpad branch

---
+++ fishfunctions/cnew.fish
@@ -0,0 +1,10 @@
+function cnew -d "Create new C project"
+    cp -r ~/dotfiles/dev/templateC $argv[1]
+    cd $argv[1]
+    sed -i "s/PROJECTNAME/$argv[1]/g" CMakeLists.txt
+    sed -i "s/PROJECTNAME/$argv[1]/g" README.org
+    git init
+    git add .
+    git commit -m "initial commit"
+    cd ..
+end
