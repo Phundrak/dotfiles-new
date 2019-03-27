0f95469f7b81d22cc6ab386836a999f071053bd8
Phuntsok Drak-pa <phundrak@phundrak.fr>
2019-03-27 15:18:27 +0100

    pulled better conna project from thinkpad branch
    Former-commit-id: 507665cd5a63f59659b594ed2d2a1b100a4fb841

--- dev/conan-project/conanfile.py
+++ dev/conan-project/conanfile.py
@@ -1,16 +1,16 @@
 from conans import ConanFile, CMake
 
-class PocoTimerConan(ConanFile):
-   settings = "os", "compiler", "build_type", "arch"
-   requires = "Poco/1.9.0@pocoproject/stable"
-   generators = "cmake", "gcc", "txt"
-   default_options = {"Poco:shared": False, "OpenSSL:shared": False}
 
-   def imports(self):
-      self.copy("*.dll", dst="bin", src="bin") # From bin to bin
-      self.copy("*.dylib*", dst="bin", src="lib") # From lib to bin
+class PROJECTNAMEConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "gtest/1.8.1@bincrafters/stable"
+    generators = "cmake", "gcc", "txt"
 
-   def build(self):
-      cmake = CMake(self)
-      cmake.configure()
-      cmake.build()
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
+        self.copy("*.dylib*", dst="bin", src="lib")  # From lib to bin
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
